e62bbde4070fe16a5b7111d16877e492234cfe9c
aaparicio <alapvi@yahoo.com>
2022-10-28 07:14:24 +0100

    Exam version hotels

---
+++ hotelsapi.py
@@ -0,0 +1,109 @@
+import requests
+
+def getHotelsByName(name):
+    url = "https://hotels4.p.rapidapi.com/locations/v3/search"
+
+    querystring = {"q":name,"locale":"en_US","langid":"1033","siteid":"300000001"}
+
+    headers = {
+        "X-RapidAPI-Key": "498beeb34fmsh7558841455add94p17d283jsn987de2c3f201",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    if (response.status_code == 200):
+        jsonResult = response.json()
+        hotelDic = {}
+        for hotel in jsonResult["sr"]:
+            hotelDic[hotel["hotelId"]] = { 
+                "name": hotel["regionNames"]["shortName"],
+                "address": hotel["hotelAddress"]["street"],
+                "city": hotel["hotelAddress"]["city"],
+                "province": hotel["hotelAddress"]["province"]
+            }
+        return hotelDic
+    return None 
+
+def getReviewById(hotelId):
+    url = "https://hotels4.p.rapidapi.com/reviews/v3/list"
+
+    payload = {
+        "currency": "USD",
+        "eapid": 1,
+        "locale": "en_US",
+        "siteId": 300000001,
+        "propertyId": hotelId,
+        "size": 20,
+        "startingIndex": 0
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "498beeb34fmsh7558841455add94p17d283jsn987de2c3f201",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    if (response.status_code == 200):
+        jsonResult = response.json()
+        #print(jsonResult)
+        reviews = []
+        for review in jsonResult["data"]["propertyInfo"]["reviewInfo"]["reviews"]:
+            if (len(review["text"])>0):
+                reviews.append(review["text"])
+        return reviews
+    return None
+
+def getHotelDetailById(hotelId):
+    url = "https://hotels4.p.rapidapi.com/properties/v2/detail"
+
+    payload = {
+        "currency": "USD",
+        "eapid": 1,
+        "locale": "en_US",
+        "siteId": 300000001,
+        "propertyId": hotelId
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "498beeb34fmsh7558841455add94p17d283jsn987de2c3f201",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    if (response.status_code == 200):
+        jsonResult = response.json()
+        #print(jsonResult)
+        hotel = {
+            "name":jsonResult["data"]["propertyInfo"]["summary"]["name"],
+            "address": jsonResult["data"]["propertyInfo"]["summary"]["location"]["address"]["addressLine"],
+            "city": jsonResult["data"]["propertyInfo"]["summary"]["location"]["address"]["city"],
+            "province": jsonResult["data"]["propertyInfo"]["summary"]["location"]["address"]["province"],
+            "value":jsonResult["data"]["propertyInfo"]["reviewInfo"]["summary"]["overallScoreWithDescriptionA11y"]["value"],
+            "reviews":[]
+        }
+        return hotel
+    return None
+    
+def getValue(hotelId):
+    url = "https://hotels4.p.rapidapi.com/reviews/v3/get-summary"
+
+    payload = {
+        "currency": "USD",
+        "eapid": 1,
+        "locale": "en_US",
+        "siteId": 300000001,
+        "propertyId": hotelId
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "498beeb34fmsh7558841455add94p17d283jsn987de2c3f201",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    if (response.status_code == 200):
+        jsonResult = response.json()
+        #print(jsonResult)
+        return jsonResult["data"]["propertyReviewSummaries"][0]["overallScoreWithDescriptionA11y"]["value"]
+    return None
---
+++ mainHotels.py
@@ -0,0 +1,70 @@
+import hotelsapi
+myHotels = {}
+
+
+while True:
+    print("I have",len(myHotels),"favourite Hotels :)")
+    print("1.- Search hotel by Name")
+    print("2.- Quality by Id")
+    print("3.- Import hotel by Id")
+    print("4.- List hotels")
+    print("5.- List reviews by Id")
+    print("6.- List hotels by value")
+    print("7.- Exit")
+    option = int(input("Select option:"))
+    if (option == 7):
+        print("Bye!")
+        break
+    elif (option == 1):
+        nameHotel = input("Enter name hotel:")
+        hotels = hotelsapi.getHotelsByName(nameHotel)
+        print("HOTELID","INFO")
+        for hotelId, hotelDetail in hotels.items():
+            print(hotelId)
+            print("\tName:",hotelDetail["name"])
+            print("\tAddress:",hotelDetail["address"])
+            print("\tcity:",hotelDetail["city"])
+            print("\tprovince:",hotelDetail["province"])
+             
+    elif (option == 2):
+        hotelId = input("Enter hotel Id:")
+        print("Quality:",hotelsapi.getValue(hotelId))
+    elif (option == 3):
+        hotelId = input("Enter hotel Id:")
+        hotel = hotelsapi.getHotelDetailById(hotelId)
+        if hotel != None:
+            print("Importing....")
+            reviews = hotelsapi.getReviewById(hotelId)
+            if reviews != None:
+                hotel["reviews"] = reviews
+                myHotels[hotelId]  = hotel
+            print("Imported successfully!")
+    elif (option == 4):
+        print("Listing Hotels")
+        for hotel,detail in myHotels.items():
+            print(hotel)
+            print("\tName:",detail["name"])
+            print("\tAddress:",detail["address"])
+            print("\tCity:",detail["city"])
+            print("\tProvince:",detail["province"])
+            print("\tValue:",detail["value"])
+            print("\tReviews (",len(detail["reviews"]),")") 
+    elif (option == 5):
+        hotelId = input("Enter hotel Id:")
+        if hotelId in myHotels:
+            print("Reviews for ",myHotels[hotelId]["name"],":")
+            for r in myHotels[hotelId]["reviews"]:
+                print("\t",r)
+    elif (option == 6):
+        quality = float(input("Enter min quality [0-10]:"))
+        print("Listing Hotels....")
+        for hotel,detail in myHotels.items():
+            if (float(detail["value"].split("/")[0]) >= quality):
+                print(hotel)
+                print("\tName:",detail["name"])
+                print("\tAddress:",detail["address"])
+                print("\tCity:",detail["city"])
+                print("\tProvince:",detail["province"])
+                print("\tValue:",detail["value"])
+    else:
+        print("Option incorrect!!!")
